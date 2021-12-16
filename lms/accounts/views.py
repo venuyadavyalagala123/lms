db4a8c05b509c760fe49dac21a64565034b14b73
venuyadavyalagala123 <venuyadavyalagala123@gmail.com>
2021-12-16 11:42:19 +0530

    login and signup form

--- lms/accounts/views.py
+++ lms/accounts/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-hii n
+def login(request):
+    return render(request,'accounts/login.html')
+
+def signup(request):
+    return render(request,'accounts/signup.html')
--- lms/lms/settings.py
+++ lms/lms/settings.py
@@ -9,12 +9,13 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
+print(TEMPLATE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'lms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
